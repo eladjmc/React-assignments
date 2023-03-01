49b70fe94c7eb6c729efe13a4010d7de22294ecb
eladjmc <eladjmc88@gmail.com>
2023-03-01 12:24:15 +0200

    added 24.1

---
+++ 24.1/src/hooks/use-counter.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export const useCounter = (initValue) => {
+  const [counter, setCounter] = useState(initValue);
+
+  const add = () => {
+    setCounter(counter + 1);
+  };
+  const sub = () => {
+    setCounter(counter - 1);
+  };
+  const multiply = () => {
+    setCounter(counter * 2);
+  };
+  const divide = () => {
+    setCounter(counter % 2 ?(counter / 2).toFixed(1) : (counter / 2));
+  };
+
+  return { counter, add, sub, multiply, divide };
+};
